99d35cbd2508024d57a0fda9b71af8272e3196cd
Aman Kumar Singh <141535006+AmanSingh-01@users.noreply.github.com>
2023-12-13 08:39:28 +0530

    Added javascript file

---
+++ script.js
@@ -0,0 +1,12 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+/*navList = document.querySelector('nav-list')*/
+/*rightNav = document.querySelector('.rightNav')*/
+
+
+burger.addEventListener('click', ()=> {
+  // rightNav.classList.toggle('v-class');
+  navbar.classList.toggle('v-class');
+  navbar.classList.toggle('h-nav');
+  /*navList.classList.toggle('v-class');*/
+})
